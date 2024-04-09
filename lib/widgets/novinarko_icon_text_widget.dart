d7795b6e5e3183b7a65dc5013a4ca156cf4b711c
Josip Kilić <neksuses@gmail.com>
2024-04-09 21:53:05 +0200

    Center the `NovinarkoIconTextWidget`

--- lib/widgets/novinarko_icon_text_widget.dart
+++ lib/widgets/novinarko_icon_text_widget.dart
@@ -29,77 +29,79 @@ class NovinarkoIconTextWidget extends StatelessWidget {
             horizontal: 20,
             vertical: verticalPadding,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 104),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 104),
 
-              if (arrowAlignment != null)
-                Align(
-                  alignment: arrowAlignment!,
-                  child: Animate(
-                    onComplete: (controller) => controller.loop(reverse: true),
-                    effects: const [
-                      MoveEffect(
-                        begin: Offset(0, -4),
-                        end: Offset(0, -8),
-                        curve: Curves.easeIn,
-                        duration: Duration(milliseconds: 750),
-                      ),
-                    ],
-                    child: Transform.rotate(
-                      angle: 0.5 * pi,
-                      child: Image.asset(
-                        NovinarkoIcons.back,
-                        fit: BoxFit.cover,
-                        color: context.colors.text,
-                        height: 40,
-                        width: 40,
+                if (arrowAlignment != null)
+                  Align(
+                    alignment: arrowAlignment!,
+                    child: Animate(
+                      onComplete: (controller) => controller.loop(reverse: true),
+                      effects: const [
+                        MoveEffect(
+                          begin: Offset(0, -4),
+                          end: Offset(0, -8),
+                          curve: Curves.easeIn,
+                          duration: Duration(milliseconds: 750),
+                        ),
+                      ],
+                      child: Transform.rotate(
+                        angle: 0.5 * pi,
+                        child: Image.asset(
+                          NovinarkoIcons.back,
+                          fit: BoxFit.cover,
+                          color: context.colors.text,
+                          height: 40,
+                          width: 40,
+                        ),
                       ),
                     ),
-                  ),
-                )
-              else
-                const SizedBox(height: 40),
+                  )
+                else
+                  const SizedBox(height: 40),
 
-              const SizedBox(height: 120),
+                const SizedBox(height: 120),
 
-              Image.asset(
-                icon,
-                fit: BoxFit.cover,
-                color: context.colors.text,
-                height: 64,
-                width: 64,
-              ),
-
-              const SizedBox(height: 36),
-
-              /// Search title
-              if (title != null)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 36),
-                  child: Text(
-                    title!,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: context.textStyles.iconTextTitle,
-                  ),
+                Image.asset(
+                  icon,
+                  fit: BoxFit.cover,
+                  color: context.colors.text,
+                  height: 64,
+                  width: 64,
                 ),
 
-              const SizedBox(height: 8),
+                const SizedBox(height: 36),
 
-              /// Search subtitle
-              if (subtitle != null)
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 60),
-                  child: Text(
-                    subtitle!,
-                    textAlign: TextAlign.center,
-                    style: context.textStyles.iconTextSubtitle,
+                /// Search title
+                if (title != null)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 36),
+                    child: Text(
+                      title!,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: context.textStyles.iconTextTitle,
+                    ),
                   ),
-                ),
-            ],
+
+                const SizedBox(height: 8),
+
+                /// Search subtitle
+                if (subtitle != null)
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 60),
+                    child: Text(
+                      subtitle!,
+                      textAlign: TextAlign.center,
+                      style: context.textStyles.iconTextSubtitle,
+                    ),
+                  ),
+              ],
+            ),
           ),
         ),
       );
