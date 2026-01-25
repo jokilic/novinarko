267165283bcd843c5328d91a4da9d29192d61270
Josip Kilić <neksuses@gmail.com>
2026-01-25 22:36:27 +0100

    Update folder feeds description

--- lib/util/string.dart
+++ lib/util/string.dart
@@ -7,35 +7,15 @@ String getFolderFeedsDescription(List<FeedModel>? feeds) {
     return 'folderDialogNoFeedsInFolder'.tr();
   }
 
-  final buffer = StringBuffer();
-  var shown = 0;
-
-  for (final feed in feeds) {
-    final name = feed.siteName ?? feed.title ?? feed.url ?? '';
-
-    if (name.isEmpty) {
-      continue;
-    }
-
-    if (shown > 0) {
-      buffer.write(', ');
-    }
-
-    buffer.write(name);
-    shown++;
-
-    if (shown == 3) {
-      break;
-    }
-  }
-
-  if (shown == 0) {
-    return 'folderDialogNoFeedsInFolder'.tr();
-  }
-
-  if (feeds.length > 3) {
-    buffer.write(', ...');
-  }
-
-  return buffer.toString();
+  return feeds
+      .map(
+        (feed) {
+          final title = feed.siteName ?? feed.title;
+          if (title?.isNotEmpty ?? false) {
+            return title;
+          }
+          return null;
+        },
+      )
+      .join(', ');
 }
